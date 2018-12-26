dd79c331b29a316530b1836b6af8f7107b182e3b
unknown <pratheeban.panch@gmail.com>
2018-12-25 22:39:04 -0500

    Adding Pet Service

---
+++ src/main/java/com/pratheeban/pet/PetRepo.java
@@ -0,0 +1,23 @@
+package com.pratheeban.pet;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface PetRepo
+		extends JpaRepository<Pet, Long> {
+
+	@Query("select p from Pet p where p.ownerId= :owner")
+	List<Pet> findAllByOwner(@Param("owner") Long owner);
+
+	@Query("select p from Pet p where p.petType= :type")
+	List<Pet> findAllByType(@Param("type") PetType petType);
+
+	@Query("select p from Pet p where p.rfid= :rfid")
+	Pet findByRfId(@Param("rfid") String rfid);
+
+	@Query("select p from Pet p")
+	List<Pet> findAll();
+}
---
+++ src/main/java/com/pratheeban/pet/PetServiceConfig.java
@@ -0,0 +1,43 @@
+package com.pratheeban.pet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Map;
+
+@Configuration
+@ComponentScan
+@EntityScan("com.pratheeban.pet")
+@EnableJpaRepositories("com.pratheeban.pet")
+@PropertySource("classpath:db/pet/db.properties")
+public class PetServiceConfig {
+
+	private static Logger logger = LoggerFactory.getLogger(PetServiceConfig.class);
+
+	@Bean
+	public DataSource dataSource() {
+		logger.debug("creating database");
+		DataSource dataSource = (new EmbeddedDatabaseBuilder())
+				.setType(EmbeddedDatabaseType.H2)
+				.addScript("classpath:db/pet/schema.sql")
+				.addScript("classpath:db/pet/data.sql").build();
+
+
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+		List<Map<String, Object>> pets = jdbcTemplate.queryForList("SELECT id FROM P_PET");
+		logger.info("System has " + pets.size() + " pets");
+
+		return dataSource;
+	}
+}
