45e6b74d6faf7d41ca1f58c8c1706751ac7724ff
unknown <pratheeban.panch@gmail.com>
2018-12-25 23:08:39 -0500

    Adding Discovery Server

---
+++ src/main/java/com/pratheeban/server/DiscoveryServer.java
@@ -0,0 +1,28 @@
+package com.pratheeban.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import java.io.IOException;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServer {
+
+	private static Logger logger = LoggerFactory.getLogger(DiscoveryServer.class);
+
+	public static void main(String[] args) throws IOException {
+		// Tell server to look for discovery.properties or discovery.yml
+		System.setProperty("spring.config.name", "discovery");
+
+		ConfigurableApplicationContext ctx = SpringApplication.run(DiscoveryServer.class, args);
+		assert (ctx != null);
+		logger.info("Started ...");
+		System.in.read();
+		ctx.close();
+	}
+
+}
